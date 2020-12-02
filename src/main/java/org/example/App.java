aa4efd028aa86bfffa4b35dc69b879c092682034
Amimul <1805056@ugrad.cse.buet.ac.bd>
2020-12-03 02:53:26 +0600

    Role based credential checker added.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -36,7 +36,27 @@ public class App
                                 InputStream inputStream = socket.getInputStream();
                                 OutputStream outputStream = socket.getOutputStream();
 
-                                credentialChecker(userDbControl, inputStream, outputStream); //Checking for role and credentials
+                                String role = credentialChecker(userDbControl, inputStream, outputStream); //Checking for credentials
+
+                                if(role != null)
+                                {
+                                    if(role.equals("Admin"))
+                                    {
+                                        adminControls(); //Controls of admin
+                                    }
+                                    else if(role.equals("Viewer"))
+                                    {
+                                        viewControls(); //Viewer controls
+                                    }
+                                    else
+                                    {
+                                        manufacturerControls(); //manufacturer controls
+                                    }
+                                }
+                                else
+                                {
+                                    credentialChecker(userDbControl, inputStream, outputStream);
+                                }
 
                             } catch (Exception e) {
                                 e.printStackTrace();
@@ -52,7 +72,20 @@ public class App
 
     }
 
-    public static void credentialChecker(UserDbControl userDbControl, InputStream inputStream, OutputStream outputStream)
+    private static void manufacturerControls()
+    {
+
+    }
+
+    private static void viewControls()
+    {
+
+    }
+
+    private static void adminControls() {
+    }
+
+    private static String  credentialChecker(UserDbControl userDbControl, InputStream inputStream, OutputStream outputStream)
     {
         try {
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
@@ -62,10 +95,42 @@ public class App
             List<String> response = userDbControl.findUser(credential.get(0), credential.get(1));
 
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            if(response.get(0).equals("false"))
+            {
+                if(response.get(3).equals("Admin"))
+                {
+                    response.add("Create a new user");
+                    response.add("Update existence user");
+                    response.add("Remove existence user");
+                }
+                else if(response.get(3).equals("Viewer"))
+                {
+                    response.add("View all cars");
+                    response.add("Search car by registration number");
+                    response.add("Search car by make and model");
+                    response.add("Buy a car");
+                }
+                else if(response.get(3).equals("Manufacturer"))
+                {
+                    response.add("View all cars");
+                    response.add("Add a car");
+                    response.add("Edit a car");
+                    response.add("Delete a car");
+                }
+            }
+
             objectOutputStream.writeObject(response);
+            objectOutputStream.flush();
+
+            if (response.get(0).equals("false"))
+                return response.get(3);
+            else
+                return null;
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
